b1f6342a54f873c9fba189d4b2a23b8207e1fedb
LucasKohler <lucaskohlermarqueswork@gmail.com>
2022-03-10 21:46:14 -0300

    refactored Utils.formatAmount() function

--- maratona-discover1/scripts.js
+++ maratona-discover1/scripts.js
@@ -100,9 +100,9 @@ const DOM = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value.replace(/\,\./g, "")) * 100;
+    value = Number(value) * 100;
 
-    return value;
+    return Math.round(value);
   },
   formatDate(date) {
     const splittedDate = date.split("-");
