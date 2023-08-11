ac9a662b8bbeeee07aba955895aa43a8c3d16331
Lucas Kohler Marques <lucaskohlermarqueswork@gmail.com>
2023-08-11 18:49:23 -0300

    Start react application

--- ignite-react/01-fundamentos-reactjs/src/App.jsx
+++ ignite-react/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,18 @@
+import { Post } from './Post'
+import { Header } from './components/Header'
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+import './styles.css'
+
+export function App() {
+	return (
+		<div>
+			<Header />
+
+			<Post
+				author="Lucas Kohler"
+				content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quos, a recusandae quasi eos itaque sapiente nemo quam qui aspernatur neque architecto animi voluptatibus. Dolores, nemo. Enim facere suscipit expedita."
+			/>
+			<Post author="Paulo Muzy" content="Eu sou corno" />
+		</div>
+	)
 }
-
-export default App
