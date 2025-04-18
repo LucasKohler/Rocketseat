fa4d42a37ab95987bd601c318350d17939d98269
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2025-04-18 12:40:05 -0300

    global css, header and start sidebar

--- Ignite/01-fundamentos-reactjs/src/App.jsx
+++ Ignite/01-fundamentos-reactjs/src/App.jsx
@@ -1,25 +1,31 @@
 import { Header } from './components/Header'
 import { Post } from './post'
+import { Sidebar } from './components/Sidebar';
 
-import './styles.css';
+import styles from './App.module.css'
+
+import './global.css';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lucas Kohler"
+            content="lorem impsum dolor suit amet conesht shajem"
+          />
 
-      <div>
-        <Post
-          author="Lucas Kohler"
-          content="lorem impsum dolor suit amet conesht shajem"
-        />
+          <Post
+            author="Kohler Lucas"
+            content="Post legal"
+          />
+        </main>
 
-        <Post
-          author="Kohler Lucas"
-          content="Post legal"
-        />
       </div>
-    </>
+    </div>
   )
 }
 
