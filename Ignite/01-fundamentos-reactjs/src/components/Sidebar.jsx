21376c595a0065ef35165e123d089579caa97dc6
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2025-04-23 12:20:53 -0300

    finalizing sidebar and creating post component

--- Ignite/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ Ignite/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,11 +8,14 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/LucasKohler.png" />
+
         <strong>Lucas Kohler Marques</strong>
         <span>Web Developer</span>
 
         <footer>
           <a href="#">
+            <PencilLine size={20} />
             Editar seu perfil
           </a>
         </footer>
