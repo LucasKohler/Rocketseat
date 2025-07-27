6a92fcd22d458ff0e347a73557aeb9071ae87502
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2025-07-27 17:01:34 -0300

    Writable and Transform stream

--- Node-rocketseat/1-FUNDAMENTOS-NODEJS/streams/fundamentals.js
+++ Node-rocketseat/1-FUNDAMENTOS-NODEJS/streams/fundamentals.js
@@ -17,7 +17,7 @@
 // process.stdin
 //  .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -37,4 +37,21 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplayByTenStream extends Writable {
+  _write(chunk, enconding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplayByTenStream())
