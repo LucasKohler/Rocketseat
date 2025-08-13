1c1b6c3e3beaa6091a3178b35e570fb18c986e0f
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2025-08-13 16:51:54 -0300

    mMock for albums

--- React-rocketseat/Gallery-Plus/src/pages/page-home.tsx
+++ React-rocketseat/Gallery-Plus/src/pages/page-home.tsx
@@ -1,13 +1,22 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <AlbumsFilter
+        albums={[
+          { id: "3421", title: "Album 1" },
+          { id: "123", title: "Album 2" },
+          { id: "456", title: "Album 3" },
+        ]}
+        className="mb-9"
+      />
+
+      <PhotosList
+        photos={[
+          {
             id: "123",
             title: "Olá mundo!",
             imageId: "portrait-tower.png",
@@ -16,34 +25,9 @@ export default function PageHome() {
               { id: "123", title: "Album 2" },
               { id: "456", title: "Album 3" },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "456",
-            title: "Olá mundo!",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "3421", title: "Album 1" },
-              { id: "123", title: "Album 2" },
-              { id: "456", title: "Album 3" },
-            ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "abc",
-            title: "Olá mundo!",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "3421", title: "Album 1" },
-              { id: "123", title: "Album 2" },
-              { id: "456", title: "Album 3" },
-            ],
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+        ]}
+      />
     </Container>
   );
 }
