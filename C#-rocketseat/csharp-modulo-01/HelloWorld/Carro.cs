0d213e4050a848845ce002aba49020b8a3630607
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2025-08-28 20:57:16 -0300

    csharp fundamentals

---
+++ C#-rocketseat/csharp-modulo-01/HelloWorld/Carro.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld;
+public class Carro
+{
+    public string Modelo { get; set; }
+    public DateOnly LancadoEm { get; set; }
+    public Cor Cor { get; set; }
+
+    /* Construtor para a classe */
+    public Carro(string modelo)
+    {
+        Modelo = modelo;
+    }
+
+    public void NomeDoModelo() => Console.WriteLine(Modelo);
+}
---
+++ C#-rocketseat/csharp-modulo-01/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main()
+    {
+        var texto = Teste(5);
+
+        Console.WriteLine("texto");
+    }
+
+    static string Teste(int numero)
+    {
+        Console.WriteLine("Teste 1");
+    
+        if (numero == 5) 
+        {
+            Console.WriteLine("Teste2");
+            return "Welisson";
+        }
+
+        Console.WriteLine("Teste 3");
+        return "Willian";
+    }
+}
